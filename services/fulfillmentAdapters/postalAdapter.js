ad0da17a2ca96724dedd4c86d2affa0e18cf56b6
davethantech <Davethantech@gmail.com>
2026-09-10 14:53:39 +0100

    Add Postal Sendoso adapter stub

---
+++ services/fulfillmentAdapters/postalAdapter.js
@@ -0,0 +1,1 @@
+module.exports={async send(){throw new Error("Postal/Sendoso adapter requires provider credentials and contract configuration.")},async status(){throw new Error("Postal adapter unavailable")}};
